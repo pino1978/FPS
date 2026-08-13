f7da1874b0b1a40f35298beb28933f05f9a09bcb
pino1978 <49398229+pino1978@users.noreply.github.com>
2026-08-13 02:20:03 +0200

    feat: register full prediction controller

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './controller';
 import { OperationsController } from './operations.controller';
+import { PredictionController } from './prediction.controller';
 import { FootballProvider, PrismaService } from './services';
 import { SettlementService } from './settlement.service';
 import { IngestionService } from './ingestion.service';
@@ -9,7 +10,7 @@ import { AnalyticsService } from './analytics.service';
 
 @Module({
   imports: [ScheduleModule.forRoot()],
-  controllers: [AppController, OperationsController],
+  controllers: [AppController, OperationsController, PredictionController],
   providers: [FootballProvider, PrismaService, SettlementService, IngestionService, AnalyticsService],
 })
 export class AppModule {}
