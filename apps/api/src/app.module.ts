755826a397ecce425eb974ac16001f304da4ed6c
pino1978 <49398229+pino1978@users.noreply.github.com>
2026-08-13 02:34:48 +0200

    feat: register value engine controller

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,6 +5,7 @@ import { OperationsController } from './operations.controller';
 import { PredictionController } from './prediction.controller';
 import { SystemController } from './system.controller';
 import { HistoryController } from './history.controller';
+import { ValueController } from './value.controller';
 import { FootballProvider, PrismaService } from './services';
 import { SettlementService } from './settlement.service';
 import { IngestionService } from './ingestion.service';
@@ -12,7 +13,7 @@ import { AnalyticsService } from './analytics.service';
 
 @Module({
   imports: [ScheduleModule.forRoot()],
-  controllers: [AppController, OperationsController, PredictionController, SystemController, HistoryController],
+  controllers: [AppController, OperationsController, PredictionController, SystemController, HistoryController, ValueController],
   providers: [FootballProvider, PrismaService, SettlementService, IngestionService, AnalyticsService],
 })
 export class AppModule {}
