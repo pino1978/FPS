77ef2f9fb284b01a79e9c5a490fb36dc94ce74a4
pino1978 <49398229+pino1978@users.noreply.github.com>
2026-08-14 00:11:24 +0200

    feat(android): add FPS WebView activity

---
+++ android/app/src/main/java/it/fps/mobile/MainActivity.java
@@ -0,0 +1,27 @@
+package it.fps.mobile;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        setContentView(webView);
+        webView.loadUrl("http://10.0.2.2:3000");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+}
