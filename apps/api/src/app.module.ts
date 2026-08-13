b64e90c09acd435e8273b24dddad3add128c68c1
pino1978 <49398229+pino1978@users.noreply.github.com>
2026-08-13 02:25:07 +0200

    feat: register system builder v2 controller

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,6 +3,7 @@ import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './controller';
 import { OperationsController } from './operations.controller';
 import { PredictionController } from './prediction.controller';
+import { SystemController } from './system.controller';
 import { FootballProvider, PrismaService } from './services';
 import { SettlementService } from './settlement.service';
 import { IngestionService } from './ingestion.service';
@@ -10,7 +11,7 @@ import { AnalyticsService } from './analytics.service';
 
 @Module({
   imports: [ScheduleModule.forRoot()],
-  controllers: [AppController, OperationsController, PredictionController],
+  controllers: [AppController, OperationsController, PredictionController, SystemController],
   providers: [FootballProvider, PrismaService, SettlementService, IngestionService, AnalyticsService],
 })
 export class AppModule {}
