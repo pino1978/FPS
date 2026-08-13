626b7323eecab5827cf34a838e3efc20730f1639
pino1978 <49398229+pino1978@users.noreply.github.com>
2026-08-13 22:44:54 +0200

    feat: enable native HTTP bridge

--- apps/mobile/capacitor.config.ts
+++ apps/mobile/capacitor.config.ts
@@ -1,10 +1,3 @@
 import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'it.fps.foresight',
-  appName: 'FPS Foresight',
-  webDir: 'dist',
-  server: { androidScheme: 'https' },
-};
-
+const config:CapacitorConfig={appId:'it.fps.foresight',appName:'FPS Foresight',webDir:'dist',server:{androidScheme:'https'},plugins:{CapacitorHttp:{enabled:true}}};
 export default config;
