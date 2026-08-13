b785fa3eaa265cc8abac55163be91e14e10db62c
pino1978 <49398229+pino1978@users.noreply.github.com>
2026-08-13 02:29:02 +0200

    feat: register v2 history controller

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppController } from './controller';
 import { OperationsController } from './operations.controller';
 import { PredictionController } from './prediction.controller';
 import { SystemController } from './system.controller';
+import { HistoryController } from './history.controller';
 import { FootballProvider, PrismaService } from './services';
 import { SettlementService } from './settlement.service';
 import { IngestionService } from './ingestion.service';
@@ -11,7 +12,7 @@ import { AnalyticsService } from './analytics.service';
 
 @Module({
   imports: [ScheduleModule.forRoot()],
-  controllers: [AppController, OperationsController, PredictionController, SystemController],
+  controllers: [AppController, OperationsController, PredictionController, SystemController, HistoryController],
   providers: [FootballProvider, PrismaService, SettlementService, IngestionService, AnalyticsService],
 })
 export class AppModule {}
