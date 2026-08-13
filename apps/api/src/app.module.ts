39f9f1b4484827603a1da1c438af929b14be5c1f
pino1978 <49398229+pino1978@users.noreply.github.com>
2026-08-13 02:16:40 +0200

    feat: register ingestion service

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,2 +1,13 @@
-import {Module} from '@nestjs/common';import {ScheduleModule} from '@nestjs/schedule';import {AppController} from './controller';import {FootballProvider,PrismaService} from './services';import {SettlementService} from './settlement.service';
-@Module({imports:[ScheduleModule.forRoot()],controllers:[AppController],providers:[FootballProvider,PrismaService,SettlementService]})export class AppModule{}
+import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
+import { AppController } from './controller';
+import { FootballProvider, PrismaService } from './services';
+import { SettlementService } from './settlement.service';
+import { IngestionService } from './ingestion.service';
+
+@Module({
+  imports: [ScheduleModule.forRoot()],
+  controllers: [AppController],
+  providers: [FootballProvider, PrismaService, SettlementService, IngestionService],
+})
+export class AppModule {}
